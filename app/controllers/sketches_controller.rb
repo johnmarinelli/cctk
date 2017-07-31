1dbccc20b155b5d58fdc2cb94da18dc8061a318f
John Marinelli <john.marinelli.dev@gmail.com>
2017-07-31 12:29:20 -0700

    Index and Show

--- app/controllers/sketches_controller.rb
+++ app/controllers/sketches_controller.rb
@@ -2,7 +2,14 @@ class SketchesController < ApplicationController
   before_action :logged_in_user, only: [:destroy]
   before_action :set_sketch, only: [:show, :edit, :update]
 
+  def index
+    @sketches = Sketch.all.paginate(page: params[:page])
+  end
+
   def show
+    @html_snippet = @sketch.snippets.find_by language: :html
+    @js_snippet = @sketch.snippets.find_by language: :javascript
+    render layout: 'compiled_sketch'
   end
 
   def new
@@ -32,7 +39,7 @@ class SketchesController < ApplicationController
     respond_to do |format|
       if @sketch.update(sketch_params)
         flash[:success] = 'Sketch updated.'
-        format.html { redirect_to @sketch }
+        format.html { redirect_to edit_sketch_url(@sketch) }
         format.json { render :show, status: :ok, location: @sketch }
       else
         flash[:danger] = 'Sketch could not be updated.'

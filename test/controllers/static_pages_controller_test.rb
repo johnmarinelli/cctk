a1ef4a3d9fb2ed146203f719051ee4dbf890da3e
John Marinelli <john.marinelli.dev@gmail.com>
2017-08-07 17:55:49 -0700

    redirect homepage to user#show if logged in

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,6 +10,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should go to user page if logged in" do
+    log_in_as users(:one), 'password1'
+    get root_path
+    assert_redirected_to users(:one)
+  end
+
   test "should get about" do
     get about_path
     assert_select "title", "About | #{@base_title}"
